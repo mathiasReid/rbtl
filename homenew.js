00d5ba6f45e49ad869b7ce7b18c199e525203bcb
mathiasReid <mathias.reidak@gmail.com>
2022-08-18 11:11:01 -0800

    fix

--- homenew.js
+++ homenew.js
@@ -1,4 +1,4 @@
-document.querySelector('closed-box').addEventListener("click", result);
+document.querySelector("img").addEventListener("click", result);
 
 function result() {
     let toggleAble = document.querySelectorAll('.toggleable');
