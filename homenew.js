fe6e9838c529130bd38f2d9d631f6f7b6f5f8356
mathiasReid <mathias.reidak@gmail.com>
2022-08-18 10:45:50 -0800

    fix

--- homenew.js
+++ homenew.js
@@ -2,6 +2,6 @@ document.querySelector("img").addEventListener("click", result);
 
 function result() {
     let toggleAble = document.querySelector('.toggleable');
-    toggleAble.classList.remove('closed-box');
-    toggleAble.classList.add('opened-box');
+    toggleAble.classList.toggle('closed-box');
+    toggleAble.classList.toggle('opened-box');
 }
